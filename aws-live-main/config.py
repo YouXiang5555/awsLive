6faa5b2436fa64cda88a0d3fe7d1641327c441a4
YouXiang5555 <132655063+YouXiang5555@users.noreply.github.com>
2023-05-05 17:04:53 +0800

    Add files via upload

---
+++ aws-live-main/config.py
@@ -0,0 +1,7 @@
+customhost = "employeedb.cbetjr4hymqv.us-east-1.rds.amazonaws.com"
+customuser = "szy_lxh_jcyx"
+custompass = "szy_lxh_jcyx"
+customdb = "employeedb"
+custombucket = "awsbucket.empdb.szylxhjcyx"
+customregion = "us-east-1"
+
